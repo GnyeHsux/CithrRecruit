fcb9e014de78c8227ec3afe2cd13c69a987f0911
xushane <xushane@outlook.com>
2016-05-30 21:05:04 +0800

    添加输入检查

--- app/src/main/java/cn/cithr/jackdraw/cithrrecruit/ui/fragment/RegisterFragment.java
+++ app/src/main/java/cn/cithr/jackdraw/cithrrecruit/ui/fragment/RegisterFragment.java
@@ -10,6 +10,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.cithr.jackdraw.cithrrecruit.R;
+import cn.cithr.jackdraw.cithrrecruit.utils.RegexUtils;
+import cn.cithr.jackdraw.cithrrecruit.utils.ToastUtils;
 
 /**
  * Created by xusha on 2016/5/24.
@@ -28,11 +30,14 @@ public class RegisterFragment extends BaseFragment {
     @BindView(R.id.btn_reg)
     Button mBtnRegNext;
 
+    private String userName = "", email = "", pwd1 = "", pwd2 = "";
+
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
         ButterKnife.bind(this, view);
-        setToolbar(mToolbar,R.string.title_register);
+        setToolbar(mToolbar, R.string.title_register);
     }
+
 
     @Override
     protected int getLayoutId() {
@@ -43,6 +48,31 @@ public class RegisterFragment extends BaseFragment {
     //注册按钮事件
     @OnClick(R.id.btn_reg)
     public void onClick() {
+        userName = mEtRegUsername.getText().toString().trim();
+        email = mEtRegEmail.getText().toString().trim();
+        pwd1 = mEtRegPwd.getText().toString().trim();
+        pwd2 = mEtRegPwdAgain.getText().toString().trim();
 
+        //检查输入是否合法
+        if (userName.equals("")) {
+            mEtRegUsername.setError("用户名不能为空");
+        } else if (email.equals("")) {
+            mEtRegEmail.setError("邮箱不能为空");
+        } else if (pwd1.equals("")) {
+            mEtRegPwd.setError("密码不能为空");
+        } else if (pwd1.length() < 6) {
+            mEtRegPwd.setError("密码不能小于6位数");
+        } else if (pwd2.equals("")) {
+            mEtRegPwdAgain.setError("密码不能为空");
+        } else if (pwd2.length() < 6) {
+            mEtRegPwd.setError("密码不能小于6位数");
+        } else if (!RegexUtils.checkEmail(email)) {
+            mEtRegEmail.setError("邮箱格式不正确");
+        } else if (!pwd1.equals(pwd2)) {
+            mEtRegPwdAgain.setError("密码不一致，请再次确认");
+        } else {
+            //执行注册事件
+            ToastUtils.showShort(getHoldingActivity(), "开始注册");
+        }
     }
 }
