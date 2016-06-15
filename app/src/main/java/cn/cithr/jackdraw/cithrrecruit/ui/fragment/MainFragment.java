fd5230c053f46aa576bf98fad1d18281f856bacb
xushane <xushane@outlook.com>
2016-06-15 23:34:19 +0800

    修改搜索按钮

--- app/src/main/java/cn/cithr/jackdraw/cithrrecruit/ui/fragment/MainFragment.java
+++ app/src/main/java/cn/cithr/jackdraw/cithrrecruit/ui/fragment/MainFragment.java
@@ -7,23 +7,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.quinny898.library.persistentsearch.SearchBox;
+import com.quinny898.library.persistentsearch.SearchResult;
+
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.cithr.jackdraw.cithrrecruit.R;
 import cn.cithr.jackdraw.cithrrecruit.ui.activity.MainActivity;
 import cn.cithr.jackdraw.cithrrecruit.ui.adapter.SimpleFragmentPagerAdapter;
+import cn.cithr.jackdraw.cithrrecruit.ui.common.HintSearchBox;
+import cn.cithr.jackdraw.cithrrecruit.utils.ToastUtils;
 
 
 /**
  * Created by xusha on 2016/5/20.
  */
 public class MainFragment extends BaseFragment {
+    private static final int SEARCH_HISTORY_SIZE = 5;   //搜索数量
 
     @BindView(R.id.tab_layout)
     TabLayout mTabLayout;
 
     @BindView(R.id.view_pager)
     ViewPager mViewPager;
+
+    @BindView(R.id.searchbox)
+    HintSearchBox mSearchBox;
+
+    private ArrayList<String> mSearchHistory;
 
     public MainFragment() {
         // Required empty public constructor
@@ -44,13 +57,70 @@ public class MainFragment extends BaseFragment {
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                //搜索按钮监听事件
+                if (id == R.id.action_search) {
+                    mSearchBox.clearSearchable();
+                    for (String his : mSearchHistory) {
+                        mSearchBox.addSearchable(
+                                new SearchResult(his, null));
+                    }
+                    mSearchBox.revealFromMenuItem(R.id.action_search, getHoldingActivity());
+
+                }
+
                 return false;
             }
         });
         ((MainActivity) getActivity()).setToolBar(toolbar);
 
         setupViewPager();
+
+        mSearchHistory = new ArrayList<>();
+        mSearchBox.setLogoText(getString(R.string.search_hint));
+        //searchBox的搜索事件
+        mSearchBox.setSearchListener(new SearchBox.SearchListener() {
+            @Override
+            public void onSearchOpened() {
+
+            }
+
+            @Override
+            public void onSearchCleared() {
+
+            }
+
+            @Override
+            public void onSearchClosed() {
+                mSearchBox.hideCircularly(getHoldingActivity());
+            }
+
+            //s为搜索关键字
+            @Override
+            public void onSearchTermChanged(String s) {
+                if (mSearchHistory.size() == SEARCH_HISTORY_SIZE) {
+                    mSearchHistory.remove(SEARCH_HISTORY_SIZE - 1);
+                }
+                if (!mSearchHistory.contains(s)) {
+                    mSearchHistory.add(0, s);
+                }
+
+            }
+
+            @Override
+            public void onSearch(String s) {
+                //启动搜索页面，传入搜索关键字
+                ToastUtils.showShort(getHoldingActivity(), s);
+            }
+
+            @Override
+            public void onResultClick(SearchResult searchResult) {
+
+            }
+        });
     }
+
 
     @Override
     protected int getLayoutId() {
